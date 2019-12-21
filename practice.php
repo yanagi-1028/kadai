239bdc008ab5710f9e7b8a351875a6d013fee842
EC2 Default User <ec2-user@ip-172-31-17-231.us-east-2.compute.internal>
2019-12-21 05:49:15 +0000

    kadai1

---
+++ practice.php
@@ -0,0 +1,42 @@
+<?php
+$a = 3;
+$b = 7;
+
+echo $a+$b;
+echo "\n";
+
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+
+echo $array_month[7];
+echo "\n";
+
+$hello = "Hello,";
+$name = "Takayuki";
+$world = "'s World!";
+
+echo $hello.$name.$world;
+echo "\n";
+
+$tech_boost = "tech";
+$tech_boost .= "boost";
+
+echo $tech_boost;
+echo "\n";
+
+$a2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+
+echo $a2018_calendar["December"];
+echo "\n";
